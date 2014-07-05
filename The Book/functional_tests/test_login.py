9df63edab59312f83a4d52e5fbe40678e0ce29ca
Wofho <ltemelakiev@gmail.com>
2014-07-05 23:05:49 +0300

    Chapter 15

--- The Book/functional_tests/test_login.py
+++ The Book/functional_tests/test_login.py
@@ -2,13 +2,14 @@ from .base import FunctionalTest
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 class LoginTest(FunctionalTest):
 
     def test_login_with_persona(self):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A Persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -17,16 +18,16 @@ class LoginTest(FunctionalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys('vernolibe@mail.bg')
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
+        self.assertIn('vernolibe@mail.bg', navbar.text)
 
     def switch_to_new_window(self, text_in_title):
         retries = 60
